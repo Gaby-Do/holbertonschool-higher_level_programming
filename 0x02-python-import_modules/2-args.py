b4224e2828c7b83127b672bd7757595cb02e2ed4
Gaby-Do <eme.gaby.do@gmail.com>
2021-12-23 08:12:53 -0800

    fixing 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 import sys
 largo = len(sys.argv)
 if largo == 1:
-    a = 'argument'
+    a = 'arguments.'
+elif largo == 2:
+    a = 'argument:'
 else:
-    a = 'arguments'
-print("{:d} {:s}:".format((largo - 1), a))
+    a = 'arguments:'
+largo2 = largo - 1
+print("{:d} {:s}".format(largo2, a))
 for i in range(1, largo):
     print("{:d}: {:s}".format(i, sys.argv[i]))
