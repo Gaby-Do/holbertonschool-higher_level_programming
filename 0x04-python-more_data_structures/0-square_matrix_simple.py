d0d810ef9bbb1407068c01b46738a295c108ff95
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-04 08:22:15 -0800

    otro modo de hacer el 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
+#    new_matrix = [[x ** 2 for x in row] for row in matrix]
+#    return new_matrix
+# o de otro modo:
+    new_matrix = []
+    for row in matrix:
+        new_list = list(map(lambda x : x * x, row))
+        new_matrix.append(new_list)
     return new_matrix
