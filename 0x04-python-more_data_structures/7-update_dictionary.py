b83c2a9975e531666cc17e116da37204ce9fa0cd
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-04 10:24:03 -0800

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return(a_dictionary.update({key: value}))
+    new_d = a_dictionary.copy()
+    return(new_d.update({key: value}))
