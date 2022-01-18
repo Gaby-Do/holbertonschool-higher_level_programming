9b55d48b6492e7a3926d3e7bf195bf7bb151c711
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-18 12:53:22 -0800

    tasks 0 and 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """this is a class named Square"""
+    def __init__(self, size):
+        self.__size = size  # private attribute
+        """Args:
+                size: size of the square
+        """
