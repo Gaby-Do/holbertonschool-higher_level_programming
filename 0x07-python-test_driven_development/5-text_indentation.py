81b33c416de1833de0a07d8a38ad8093d797659e
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-26 11:58:53 -0800

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,22 +10,17 @@ def text_indentation(text):
         Arg:
             text(str): text to be printed
     """
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-    flag = 0
-    for char in text:
-        if flag == 0:
-            if char == '.' or char == ':' or char == '?':
-                print(char)
-                print()
-                flag = 1
-            else:
-                print(char, end="")
-                flag = 0
+
+    text_update = str(text)
+    after_new_line = False
+    for c in text_update:
+        if after_new_line:
+            if c == " ":
                 continue
-        if flag == 1:
-            if char == '.' or char == ':' or char == '?' or char == ' ':
-                continue    
-            else:
-                print(char, end="")
-                flag = 0
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
