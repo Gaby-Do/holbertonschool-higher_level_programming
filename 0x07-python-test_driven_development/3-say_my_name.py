2a68c908c546fe1020ed1206b048c06f540c03c3
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-26 11:37:49 -0800

    fixing 3-say...???

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-#2. Say my name
-#function that prints My name is <first name> <last name>
+"""
+2. Say my name
+function that prints My name is <first name> <last name>
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    #prints first_name and last_name,
-    #checks if they are strings,
-    #if not raises a TypeError with a msg
+    """
+    prints first_name and last_name,
+    checks if they are strings,
+    if not raises a TypeError with a msg
+    """
 
-    #check if arguments are strings
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
