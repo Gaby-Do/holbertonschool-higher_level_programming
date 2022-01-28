341e8ee92b127a1b17c197d0b55eeadfafdfb065
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-28 13:01:23 -0800

    experimenting

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,24 +5,24 @@ function that prints a text with 2 new lines
 after each of these characters: ., ? and :
 """
 
-
 def text_indentation(text):
     """
         Arg:
             text(str): text to be printed
     """
-
     if type(text) is not str:
         raise TypeError("text must be a string")
-    after_new_line = False
-    for c in text:
-        if after_new_line:
-            if c == " ":
-                continue
-            after_new_line = False
-        if c == '.' or c == '?' or c == ':':
-            print(c)
-            print("")
-            after_new_line = True
+
+    text = text.strip()
+    i = 0
+    while i < len(text):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i])
+            print()
+            while text[i + 1] == ' ':
+                i += 1
         else:
-            print(c, end="")
+            print(text[i], end='')
+        i += 1
+    if i == len(text):
+        print()
