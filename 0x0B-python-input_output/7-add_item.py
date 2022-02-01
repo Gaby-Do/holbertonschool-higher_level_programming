d0115afb07631fa33ce8b90eb8f6043d41229ec7
Gaby-Do <eme.gaby.do@gmail.com>
2022-02-01 12:28:30 -0800

    fixing 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,4 +14,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-save_to_json_file(sys.argv[1:], 'add_item.json')
+try:
+    lista = load_from_json_file('add_item.json')
+    save_to_json_file(lista + sys.argv[1:], 'add_item.json')
+except Exception:
+    save_to_json_file(sys.argv[1:], 'add_item.json')
