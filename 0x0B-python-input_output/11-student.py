4c00caa31f4a8d4e24c9730d29d5917f15626964
Gaby-Do <eme.gaby.do@gmail.com>
2022-02-02 07:24:57 -0800

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+10. Student to JSON with filter
+Idem 9. plus:
+If attrs is a list of strings, only attribute names contained
+in this list must be retrieved.
+"""
+
+
+class Student:
+    """
+    Student is defined by:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            Retrieves a dictionary representation
+            of a Student instance
+            Arg: attrs: attributes to retrieve
+        """
+        if type(attrs) is not list:
+            return self.__dict__
+        aux = {}
+        for key in attrs:
+            if type(key) is not str:
+                return self.__dict__
+            if key in self.__dict__:
+                aux[key] = self.__dict__[key]
+        return aux
+
+    def reload_from_json(self, json):
+        """
+            Replaces all attributes of the Student instance
+        """
+        self.__dict__ = json
