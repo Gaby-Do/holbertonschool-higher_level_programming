24e4389fcaee6ec5a6a5744e830779d11a8b8b93
Gaby-Do <eme.gaby.do@gmail.com>
2022-02-08 05:24:09 -0800

    tests: rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,10 +14,6 @@ class Test_Rectangle(unittest.TestCase):
 
     def test_rectangle(self):
         self.assertRaises(TypeError, Rectangle, ())
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.id, 19)
-        r2 = Rectangle(1, 2, 3)
-        self.assertEqual(r2.id, 20)
 
 # Tests for width:
 
