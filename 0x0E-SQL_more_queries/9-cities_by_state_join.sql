c908cffd6080c9eaf25dc52df0f1f7a5b7ae7886
Gaby-Do <eme.gaby.do@gmail.com>
2022-03-09 11:03:57 -0800

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- 9. Cities by States
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name FROM cities
-RIGHT JOIN states ON states.id = cities.state_id
+ LEFT JOIN states ON states.id = cities.state_id
