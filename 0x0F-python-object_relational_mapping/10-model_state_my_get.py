569ebee483a80beee7cf5864f1097c7e6c6cbd2c
Gaby-Do <eme.gaby.do@gmail.com>
2022-04-09 13:37:33 -0700

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+10. Get a state
+prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa
+use the module SQLAlchemy
+code not executed when imported
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    state = session.query(State).filter(State.name.like(sys.argv[4])).first()
+    if state is None:
+        print("Not found")
+    else:
+        print("{}".format(state.id))
+    session.close()
