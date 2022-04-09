d38a5fb303e20c4fb0ebec92a5690f0a526dbe17
Gaby-Do <eme.gaby.do@gmail.com>
2022-04-09 11:59:40 -0700

    fixes to task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             port=3306,
             user=argv[1],
             passwd=argv[2],
-            db="hbtn_0e_0_usa")
+            db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
